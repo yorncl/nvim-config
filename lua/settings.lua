812c567617355efc7a65851ba031c62e96224e75
yorn <yornclaudel@gmail.com>
2022-05-26 02:38:49 +0200

    finally removed background, pretty now

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,7 +2,7 @@ vim.wo.number = true
 
 -- Theme and transparency
 vim.cmd("colorscheme gruvbox")
-vim.cmd("au ColorScheme * hi Normal ctermbg=none guibg=none")
+vim.cmd(":hi Normal ctermbg=none guibg=none")
 
 -- Mouse
 vim.cmd("set mouse=a")
