0937fd6ee118a1f512a8a09530803f2d5adcc967
Martin Claudel <yornclaudel@gmail.com>
2022-06-05 22:35:08 +0200

    added relativnumber

--- lua/settings.lua
+++ lua/settings.lua
@@ -7,6 +7,9 @@ vim.cmd(":hi Normal ctermbg=none guibg=none")
 -- Mouse
 vim.cmd("set mouse=a")
 
+-- Relative numbers
+vim.cmd("set relativenumber")
+
 -- Timeout (for which key)
 vim.api.nvim_set_option('timeoutlen', 500)
 
